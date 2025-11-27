764002fb96de02b4a40205fa760ccaca22617ccf
MRF <74323188+Cachton@users.noreply.github.com>
2025-11-26 19:01:02 -0500

    v0.4
    First look at a graph in the bottom left corner meant to tell the total wins on every map in the pool during season 1
    
    practicing with graphs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,63 @@
-import { animate } from 'animejs';
 
-animate('.dot', {
-    scale: 1.1,
+import Chart from "https://esm.sh/chart.js/auto";
+
+let graphique = new Chart(document.querySelector("canvas"), {
+    type: "radar",
+    data: {
+        labels: [
+            "Anubis",
+            "Inferno",
+            "Mirage",
+            "Train",
+            "Dust II",
+            "Nuke",
+            "Ancient"
+        ],
+        datasets: [
+            {
+                label: "TOTAL MAP WINS",
+                data: [12, 12, 12, 12, 12, 15, 12],
+                backgroundColor: "rgba(255, 255, 255, 1)",
+                pointBackgroundColor: "rgba(0, 0, 0, 1)",
+                borderWidth: 10
+            }
+
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            tooltip: {
+                enabled: false
+            },
+            title: {
+                display: false,
+                text: "WINRATE"
+            }
+        },
+        scales: {
+            r: {
+                angleLines: {
+                    color: "rgba(0, 0, 0, 0.1)"
+                },
+                grid: {
+                    circular: false,
+                    color: "rgba(0, 0, 0, 0.1)"
+                },
+                ticks: { stepSize: 10, showLabelBackdrop: false }
+            }
+        }
+    }
 });
 
+// Décommenter pour animer
+//
+// setInterval(() => {
+//   for (let ds of graphique.data.datasets) {
+//     for (let i = 0; i < ds.data.length; i++) {
+//       ds.data[i] = Math.round(Math.random() * 40 + 10);
+//     }
+//   }
+//   graphique.update();
+// }, 1000);
